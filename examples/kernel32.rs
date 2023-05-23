6db445dfb3f85f2880b2d0147247ecb552c7abdb
Brandon Ros <brandonros1@gmail.com>
2023-05-23 16:01:19 -0400

    get rid of log package, get rid of libc reference, temporarily call out minidl dependency more clearly

--- examples/kernel32.rs
+++ examples/kernel32.rs
@@ -1,9 +1,9 @@
 #![allow(non_upper_case_globals)]
 
-use crate::libc::*;
 use once_cell::sync::Lazy;
 use retour::{GenericDetour, Function};
 use winapi::{shared::{ntdef::*, minwindef::*}};
+use minidl::Library;
 
 type fn_LoadLibraryA = extern "system" fn(LPCSTR) -> HMODULE;
 type fn_LoadLibraryW = extern "system" fn(LPCWSTR) -> HMODULE;
@@ -16,33 +16,61 @@ static hook_LoadLibraryExA: Lazy<GenericDetour<fn_LoadLibraryExA>> = Lazy::new(|
 static hook_LoadLibraryExW: Lazy<GenericDetour<fn_LoadLibraryExW>> = Lazy::new(|| build_detour("kernel32.dll\0", "LoadLibraryExW\0", our_LoadLibraryExW));
 
 fn build_detour<T: Function>(lpFileName: &str, lpProcName: &str, detour_fn: T) -> GenericDetour<T> {
-  let library = minidl::Library::load(lpFileName).unwrap();
+  let library = Library::load(lpFileName).unwrap();
   let ori = unsafe { library.sym(lpProcName).unwrap() };
   return unsafe { 
     GenericDetour::new(ori, detour_fn).unwrap()
   };
 }
 
+fn wcslen(s: *const u16) -> usize {
+  let mut i = 0;
+  unsafe {
+      while *s.offset(i) != 0 {
+          i += 1;
+      }
+  }
+  i as usize
+}
+
+fn lpcstr_to_rust_string(input: LPCSTR) -> String {
+  if input.is_null() {
+    return String::from("(null)");
+  }
+  let length = unsafe { strlen(input) };
+  let slice: &[u8] = unsafe { std::slice::from_raw_parts(input as *const u8, length) };
+  return String::from_utf8(slice.to_vec()).unwrap();
+}
+
+fn lpcwstr_to_rust_string(input: LPCWSTR) -> String {
+  if input.is_null() {
+    return String::from("(null)");
+  }
+  let length = unsafe { wcslen(input) };
+  let slice = unsafe { std::slice::from_raw_parts(input, length) };
+  return String::from_utf16_lossy(slice);
+}
+
 extern "system" fn our_LoadLibraryA(lpFileName: LPCSTR) -> HMODULE {
-  log::info!("our_LoadLibraryA lpFileName = {}", lpcstr_to_rust_string(lpFileName));
+  println!("our_LoadLibraryA lpFileName = {}", lpcstr_to_rust_string(lpFileName));
   unsafe { hook_LoadLibraryA.disable().unwrap() };
   let ret_val = hook_LoadLibraryA.call(lpFileName);
-  log::info!("our_LoadLibraryA lpFileName = {} ret_val = {:p}", lpcstr_to_rust_string(lpFileName), ret_val);
+  println!("our_LoadLibraryA lpFileName = {} ret_val = {:p}", lpcstr_to_rust_string(lpFileName), ret_val);
   unsafe { hook_LoadLibraryA.enable().unwrap() };
   return ret_val;
 }
 
 extern "system" fn our_LoadLibraryW(lpFileName: LPCWSTR) -> HMODULE {
-  log::info!("our_LoadLibraryW lpFileName = {}", lpcwstr_to_rust_string(lpFileName));
+  println!("our_LoadLibraryW lpFileName = {}", lpcwstr_to_rust_string(lpFileName));
   unsafe { hook_LoadLibraryW.disable().unwrap() };
   let ret_val = hook_LoadLibraryW.call(lpFileName);
-  log::info!("our_LoadLibraryW lpFileName = {} ret_val = {:p}", lpcwstr_to_rust_string(lpFileName), ret_val);
+  println!("our_LoadLibraryW lpFileName = {} ret_val = {:p}", lpcwstr_to_rust_string(lpFileName), ret_val);
   unsafe { hook_LoadLibraryW.enable().unwrap() };
   return ret_val;
 }
 
 extern "system" fn our_LoadLibraryExA(lpLibFileName: LPCSTR, hFile: HANDLE, dwFlags: DWORD) -> HMODULE {
-  log::info!(
+  println!(
     "our_LoadLibraryExA lpLibFileName = {} hFile = {:p} dwFlags = {:08x}",
     lpcstr_to_rust_string(lpLibFileName),
     hFile,
@@ -50,7 +78,7 @@ extern "system" fn our_LoadLibraryExA(lpLibFileName: LPCSTR, hFile: HANDLE, dwFl
   );
   unsafe { hook_LoadLibraryExA.disable().unwrap(); }
   let ret_val = hook_LoadLibraryExA.call(lpLibFileName, hFile, dwFlags);
-  log::info!(
+  println!(
     "our_LoadLibraryExA lpLibFileName = {} hFile = {:p} dwFlags = {:08x} ret_val = {:p}",
     lpcstr_to_rust_string(lpLibFileName),
     hFile,
@@ -62,7 +90,7 @@ extern "system" fn our_LoadLibraryExA(lpLibFileName: LPCSTR, hFile: HANDLE, dwFl
 }
 
 extern "system" fn our_LoadLibraryExW(lpLibFileName: LPCWSTR, hFile: HANDLE, dwFlags: DWORD) -> HMODULE {
-  log::info!(
+  println!(
     "our_LoadLibraryExW lpLibFileName = {} hFile = {:p} dwFlags = {:08x}",
     lpcwstr_to_rust_string(lpLibFileName),
     hFile,
@@ -70,7 +98,7 @@ extern "system" fn our_LoadLibraryExW(lpLibFileName: LPCWSTR, hFile: HANDLE, dwF
   );
   unsafe { hook_LoadLibraryExW.disable().unwrap(); }
   let ret_val = hook_LoadLibraryExW.call(lpLibFileName, hFile, dwFlags);
-  log::info!(
+  println!(
     "our_LoadLibraryExW lpLibFileName = {} hFile = {:p} dwFlags = {:08x} ret_val = {:p}",
     lpcwstr_to_rust_string(lpLibFileName),
     hFile,
@@ -79,24 +107,6 @@ extern "system" fn our_LoadLibraryExW(lpLibFileName: LPCWSTR, hFile: HANDLE, dwF
   );
   unsafe { hook_LoadLibraryExW.enable().unwrap(); }
   return ret_val;
-}
-
-fn lpcstr_to_rust_string(input: LPCSTR) -> String {
-  if input.is_null() {
-    return String::from("(null)");
-  }
-  let length = strlen(input);
-  let slice: &[u8] = unsafe { std::slice::from_raw_parts(input as *const u8, length) };
-  return String::from_utf8(slice.to_vec()).unwrap();
-}
-
-fn lpcwstr_to_rust_string(input: LPCWSTR) -> String {
-  if input.is_null() {
-    return String::from("(null)");
-  }
-  let length = wcslen(input);
-  let slice = unsafe { std::slice::from_raw_parts(input, length) };
-  return String::from_utf16_lossy(slice);
 }
 
 pub fn init() {
